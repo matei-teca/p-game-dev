9c784f65b6a7ff3019a05036d73ee85547692921
Valentin1289 <valibaiacof1289@gmail.com>
2023-03-02 17:26:32 +0200

    starter page change gif

--- client/src/components/StarterPage.js
+++ client/src/components/StarterPage.js
@@ -1,15 +1,17 @@
 export default function StarterPage() {
   return (
     <div className="locations-app">
-
-    <div>
-      <img
-        src={"https://media.tenor.com/fSsxftCb8w0AAAAi/pikachu-running.gif"}
+      <div>
+        <img
+          style={{
+            width: "40%",
+          }}
+          src={"../images/pokemon-pikachu-running-fdcgna00aeogxcjz.gif"}
         />
-      <p className="loadingMessage">The pokemons are beeing fetched</p>
-      <p className="loadingBar"></p>
-      <progress id="file" max="520" value="0"></progress>
+        <p className="loadingMessage">The pokemons are beeing fetched</p>
+        <p className="loadingBar"></p>
+        <progress id="file" max="520" value="0"></progress>
+      </div>
     </div>
-        </div>
   );
 }
